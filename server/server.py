1d9d0ce38e9b5ec1aa3a50943723f69419345166
asciishell <asciishell@yandex.ru>
2019-05-23 16:48:40 +0300

    Deploy to docker compose

--- server/server.py
+++ server/server.py
@@ -15,6 +15,7 @@ class Root(object):
 
 if __name__ == '__main__':
     cherrypy.config.update({
+        'server.socket_host': '0.0.0.0',
         'server.socket_port': 8080,
         'tools.proxy.on': True,
     })
